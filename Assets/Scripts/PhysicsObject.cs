91e7ac0acd3cf3e1ff894eeb3516e0c95879f78f
Patrick <s198018@students.aie.edu>
2020-09-10 12:52:24 -0500

    Created a scene with a box that changes color
    changes color dependent on whether it is awake or asleep

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicsObject : MonoBehaviour
+{
+    public Material awakeMaterial = null;
+    public Material asleepMaterial = null;
+
+    private Rigidbody _rigidbody = null;
+    private Material _material = null;
+
+    private bool wasSleeping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _material = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(_rigidbody.IsSleeping() && !wasSleeping && asleepMaterial != null)
+        {
+            wasSleeping = true;
+            _material = asleepMaterial;
+        }
+
+        if (!_rigidbody.IsSleeping() && wasSleeping && awakeMaterial != null)
+        {
+            wasSleeping = false;
+            _material = awakeMaterial;
+        }
+    }
+}
