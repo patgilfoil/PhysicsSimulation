6220b3f6add0ec88da57aad22ede2f1ea0982714
Patrick <s198018@students.aie.edu>
2020-09-10 17:02:02 -0500

    chair

---
+++ Assets/Scripts/CannonballBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CannonballBehavior : MonoBehaviour
+{
+    public float forceOnFire = 300;
+
+    private bool _canFire = false;
+
+    private Rigidbody _rigidbody = null;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && _canFire)
+        {
+            _rigidbody.isKinematic = false;
+            _rigidbody.AddForce(transform.forward * forceOnFire);
+            _canFire = false;
+        }
+    }
+}
